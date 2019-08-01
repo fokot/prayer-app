5b3b481cd0f68cca58f7b6daae394961961df417
fokot <frantisek.kocun@gmail.com>
2019-08-01 17:38:03 +0200

    main refactor

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,22 +1,6 @@
 import React from 'react';
 import Main from './app/Main';
-import {storeEveryting, getAllPrayerIds} from "./app/utils/PrayerStorage";
-import prayers from "./app/utils/Prayers";
 
-
-export default class App extends React.Component {
-
-  async newPrayers() {
-    const ids = await getAllPrayerIds();
-    if(ids.length < 1)
-      storeEveryting(prayers);
-  }
-
-  componentDidMount(): void {
-    this.newPrayers();
-  }
-
-  render() {
+export default function () {
     return <Main/>;
-  }
 }
